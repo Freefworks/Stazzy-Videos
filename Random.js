61cb6ecaf1858be39812876507f3f46da99716e6
Stazzy <freef@tutamail.com>
2025-04-19 04:35:33 -0400

    Add files via upload

---
+++ Random.js
@@ -0,0 +1,6 @@
+var sites = [
+'video-1.html',
+'video-2.html',
+'video-3.html',
+'video-4.html'
+]; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
