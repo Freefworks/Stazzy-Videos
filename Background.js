193003524f3e162a86731c75c17c5cea2083ff56
Stazzy <freef@tutamail.com>
2026-01-20 22:51:35 -0500

    Add files via upload

---
+++ Background.js
@@ -0,0 +1,1 @@
+function randombg(){var random= Math.floor(Math.random() * 10) + 0; var bigSize = ["url('sky1.png')", "url('sky2.png')", "url('sky3.png')", "url('sky4.png')", "url('sky5.png')", "url('sky6.png')", "url('sky7.png')", "url('sky8.png')", "url('sky9.png')", "url('sky10.png')",]; document.getElementById("random").style.backgroundImage=bigSize[random];}
